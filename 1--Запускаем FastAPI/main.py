ff6cf4be7ca1bbc702fe2064c2ed1b3c32571c60
Ümit Aşan <tgrandis@unknownea07de213aa2.home>
2024-11-14 01:45:32 +0300

    Запускаем FastAPI

---
+++ 1--Запускаем FastAPI/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(title="FastAPI Stepik Courses")
+
+
+
+@app.get("/get_hotels")
+def get_hotels():
+    return "5 Yildizli Antalya oteli"
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
